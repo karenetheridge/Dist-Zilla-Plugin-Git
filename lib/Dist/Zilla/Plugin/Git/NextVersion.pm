karenetheridge/Dist-Zilla-Plugin-Git

commit 0bbde07d75b9d60e582c870c1ca7c58b9e65c44f

clean up dumped configs
booleans are canonicalized to 0,1
lists are sorted - this will improve the consistency between builds, which helps distribution packagers

lib/Dist/Zilla/Plugin/Git/NextVersion.pm
@@ -121,7 +121,8 @@ around dump_config => sub
     my $config = $self->$orig;
 
     $config->{+__PACKAGE__} = {
-        map { $_ => $self->$_ } qw(version_regexp first_version version_by_branch),
+        (map { $_ => $self->$_ } qw(version_regexp first_version)),
+        $self->version_by_branch ? 1 : 0,
     };
 
     return $config;

lib/Dist/Zilla/Plugin/Git/Check.pm
@@ -34,6 +34,7 @@ around dump_config => sub
     my $config = $self->$orig;
 
     $config->{+__PACKAGE__} = {
+        # build_warnings does not affect the build outcome; do not need to track it
         untracked_files => $self->untracked_files,
     };
 

lib/Dist/Zilla/Plugin/Git/Commit.pm
@@ -43,7 +43,8 @@ around dump_config => sub
     my $config = $self->$orig;
 
     $config->{+__PACKAGE__} = {
-        map { $_ => $self->$_ } qw(commit_msg time_zone add_files_in),
+        commit_msg => $self->commit_msg,
+        add_files_in => [ sort @{ $self->add_files_in } ],
     };
 
     return $config;

lib/Dist/Zilla/Plugin/Git/CommitBuild.pm
@@ -84,8 +84,9 @@ around dump_config => sub
     my $config = $self->$orig;
 
     $config->{+__PACKAGE__} = {
-        map { $_ => $self->$_ }
-            qw(branch release_branch message release_message build_root multiple_inheritance),
+        (map { $_ => $self->$_ }
+            qw(branch release_branch message release_message build_root)),
+        multiple_inheritance => $self->multiple_inheritance ? 1 : 0,
     };
 
     return $config;

lib/Dist/Zilla/Plugin/Git/GatherDir.pm
@@ -129,7 +129,7 @@ around dump_config => sub
     my $config = $self->$orig;
 
     $config->{+__PACKAGE__} = {
-        include_untracked => $self->include_untracked,
+        include_untracked => $self->include_untracked ? 1 : 0,
     };
 
     return $config;

lib/Dist/Zilla/Plugin/Git/Push.pm
@@ -42,7 +42,8 @@ around dump_config => sub
     my $config = $self->$orig;
 
     $config->{+__PACKAGE__} = {
-        map { $_ => $self->$_ } qw(push_to remotes_must_exist),
+        push_to => $self->push_to,
+        remotes_must_exist => $self->remotes_must_exist ? 1 : 0,
     };
 
     return $config;

lib/Dist/Zilla/Plugin/Git/Tag.pm
@@ -56,7 +56,8 @@ around dump_config => sub
     my $config = $self->$orig;
 
     $config->{+__PACKAGE__} = {
-        map { $_ => $self->$_ } qw(tag_format tag_message time_zone branch signed tag),
+        (map { $_ => $self->$_ } qw(tag_format tag_message changelog branch tag)),
+        signed => $self->signed ? 1 : 0,
     };
 
     return $config;

lib/Dist/Zilla/Role/Git/DirtyFiles.pm
@@ -93,7 +93,8 @@ around dump_config => sub
     my $config = $self->$orig;
 
     $config->{+__PACKAGE__} = {
-        map { $_ => $self->$_ } qw(allow_dirty allow_dirty_match changelog),
+        (map { $_ => [ sort @{ $self->$_ } ] } qw(allow_dirty allow_dirty_match)),
+        changelog => $self->changelog,
     };
 
     return $config;

commit 76704ac2cf0c3adf0ec6a78c3b9aac1980946087

Dump version_by_branch correctly
  Fixes bug introduced in 0bbde07d75b9d60e582c870c1ca7c58b9e65c44f
  (Bug was never in a released version.)

lib/Dist/Zilla/Plugin/Git/NextVersion.pm
@@ -123,7 +123,7 @@ around dump_config => sub
 
     $config->{+__PACKAGE__} = {
         (map { $_ => $self->$_ } qw(version_regexp first_version)),
-        $self->version_by_branch ? 1 : 0,
+        version_by_branch => $self->version_by_branch ? 1 : 0,
     };
 
     return $config;