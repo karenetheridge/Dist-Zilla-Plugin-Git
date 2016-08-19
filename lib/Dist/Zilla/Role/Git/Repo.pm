dafef2d291b27f40d71c33dfa927916e8f029337
Karen Etheridge <ether@cpan.org>
2016-08-18 18:24:22 -0700

    log "git --version" into metadata

--- lib/Dist/Zilla/Role/Git/Repo.pm
+++ lib/Dist/Zilla/Role/Git/Repo.pm
@@ -62,6 +62,7 @@ around dump_config => sub
 
     $config->{+__PACKAGE__} = {
         repo_root => $self->repo_root,
+        'git --version' => $self->git('version'),
     };
 
     return $config;
