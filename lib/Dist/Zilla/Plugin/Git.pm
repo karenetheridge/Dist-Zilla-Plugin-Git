9d36216ccc82a8dcc4cfb9b4183156e031ba6168
Karen Etheridge <ether@cpan.org>
2018-06-02 18:17:41 -0700

    remove SEE ALSO section
    search.cpan.org is about to be turned off, annocpan and cpan ratings are
    long defunct, and the mailing list, git repository and bug queue are
    mentioned in the SUPPORT section and CONTRIBUTING file added via my
    plugin bundle, as well as in metadata (META.json).

--- lib/Dist/Zilla/Plugin/Git.pm
+++ lib/Dist/Zilla/Plugin/Git.pm
@@ -80,36 +80,4 @@ using L<Version::Next>.
 
 =back
 
-=head1 SEE ALSO
-
-You can look for information on this module at:
-
-=over 4
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Dist-Zilla-Plugin-Git>
-
-=item * See open / report bugs
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Dist-Zilla-Plugin-Git>
-
-=item * Mailing-list (same as L<Dist::Zilla>)
-
-L<http://www.listbox.com/subscribe/?list_id=139292>
-
-=item * Git repository
-
-L<http://github.com/madsen/dist-zilla-plugin-git>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Dist-Zilla-Plugin-Git>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Dist-Zilla-Plugin-Git>
-
-=back
-
 =cut
